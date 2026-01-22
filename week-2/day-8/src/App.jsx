0a379a5329bf07599febabb2ea4578bf6977db6d
Joshua Abah <donjoe270@gmail.com>
2026-01-22 03:35:28 +0100

    Day 12: useEffect, side effects, and localStorage

--- week-2/day-8/src/App.jsx
+++ week-2/day-8/src/App.jsx
@@ -1,52 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    age: "",
+  const [count, setCount] = useState(() => {
+    return Number(localStorage.getItem("count")) || 0;
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  useEffect(() => {
+    localStorage.setItem("count", count);
+  }, [count]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); // prevent page reload
-    alert(`Form Submitted!\nName: ${formData.name}\nEmail: ${formData.email}\nAge: ${formData.age}`);
-    setFormData({ name: "", email: "", age: "" }); // reset form
-  };
+  useEffect(() => {
+    console.log("Count changed");
+  }, [count]);
+  
 
   return (
     <div className="container">
-      <h1>Day 11 – React Forms</h1>
+      <h1>Day 12 – useEffect & localStorage</h1>
+      <p>Count: {count}</p>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          name="age"
-          placeholder="Age"
-          value={formData.age}
-          onChange={handleChange}
-        />
-
-        <button type="submit">Submit</button>
-      </form>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
